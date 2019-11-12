ce9cca710819962a59a49e1d1bc5e5da1468aea4
Ovidiu Petridean <opetridean@sdl.com>
2019-11-12 21:33:08 +0200

    workshop

---
+++ src/main/java/org/itdays/demos/twitter/FilterStreamExample.java
@@ -0,0 +1,79 @@
+package org.itdays.demos.twitter;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Client;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.connect.json.JsonDeserializer;
+import org.apache.kafka.streams.StreamsConfig;
+
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+
+public class FilterStreamExample {
+
+    public static void run(String consumerKey, String consumerSecret, String token, String secret) throws InterruptedException, IOException {
+        Properties props = new Properties();
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ProducerConfig.CLIENT_ID_CONFIG, "random-id");
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer producer = new KafkaProducer<>(props);
+
+        BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
+        StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
+        // add some track terms
+        endpoint.trackTerms(Lists.newArrayList("twitterapi", "#trump"));
+
+        Authentication auth = new OAuth1(consumerKey, consumerSecret, token, secret);
+        // Authentication auth = new BasicAuth(username, password);
+
+        // Create a new BasicClient. By default gzip is enabled.
+        Client client = new ClientBuilder()
+                .hosts(Constants.STREAM_HOST)
+                .endpoint(endpoint)
+                .authentication(auth)
+                .processor(new StringDelimitedProcessor(queue))
+                .build();
+
+        // Establish a connection
+        client.connect();
+        ObjectMapper mapper = new ObjectMapper();
+
+        // Do whatever needs to be done with messages
+        for (int msgRead = 0; msgRead < 1000; msgRead++) {
+            String msg = queue.take();
+            System.out.println(msg);
+            JsonNode actualObj = mapper.readTree(msg);
+            ProducerRecord producerRecord = new ProducerRecord("tweets", actualObj.get("text").textValue());
+            producer.send(producerRecord);
+        }
+
+        client.stop();
+    }
+
+  public static void main(String[] args) {
+    try {
+      FilterStreamExample.run("hIjSRdv6WMlSetmTMdN5sdJdz", "4x3hhvux1UPzvrOprUhPGYSPr8lc80gQKx0GAZesx0WTNiw7dZ",
+              "730361888014479360-JWBRI7GvFjFvFGRIkwtDZDUsbB9epoW", "8dopHoNlqgEBNcoK1NBjSQG3BXZYUzL4RnOq9pWv3G6xt");
+    } catch (InterruptedException | IOException e) {
+      System.out.println(e);
+    }
+  }
+}
